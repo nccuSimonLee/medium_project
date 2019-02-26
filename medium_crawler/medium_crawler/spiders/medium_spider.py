6e1b5024a43f62dccaa5983551c094344a00fa85
nccuSimonLee <simon2020291@gmail.com>
2019-02-26 19:20:22 +0800

    [add] get the basic info from each posts.

--- medium_crawler/medium_crawler/spiders/medium_spider.py
+++ medium_crawler/medium_crawler/spiders/medium_spider.py
@@ -9,5 +9,14 @@ class MediumSpiderSpider(scrapy.Spider):
 
     def parse(self, response):
         print("Processing.." + response.url)
-        for res in response.css(".u-borderLighter"):
-            print(res.attrib["href"])
+        posts = response.css(".u-borderLighter")
+        for post in posts:
+            url = post.attrib["href"]
+            yield scrapy.Request(url, callback=self.parse_detail_page)
+
+    def parse_detail_page(self, response):
+        title = response.css("h1::text").extract()[0] # extract 回傳 list
+        author = response.css(".ds-link::text").extract()[0]
+        date = response.css(".ui-caption > time").attrib["datetime"]
+        reading_time = response.css(".readingTime").attrib["title"]
+        cotent = response.css(".postArticle-content > section")
